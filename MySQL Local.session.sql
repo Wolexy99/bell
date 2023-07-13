4396f4144faffff2ba427863a1733cd6453d5641
Your Name <you@example.com>
2023-07-13 09:48:54 +0100

    new file:   .vscode/launch.json 	new file:   .vscode/settings.json 	new file:   2D-Transforms.css 	new file:   bell.css 	new file:   bell.html

---
+++ MySQL Local.session.sql
@@ -0,0 +1,14 @@
+CREATE TABLE make_table(
+    sometable VARCHAR(100)
+);
+
+INSERT INTO make_table(sometable)
+VALUES ('abc');
+
+SELECT sometable
+FROM make_table;
+
+INSERT INTO make_table (sometable)
+VALUES ('sometable:varcharTablesTablesINSERT INTO make_table (sometable)
+VALUES ('sometable:varchar');');INSERT INTO make_table(sometable)
+VALUES ('def');
